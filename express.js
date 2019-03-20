c59db884255c3b5d0149a88fa16ecc11f8621567
jodamu <jodamu90@gmail.com>
2019-03-20 11:49:46 -0500

    proyecto1

--- express.js
+++ express.js
@@ -1,8 +1,6 @@
 var express = require('express')
 var app = express()
  
-app.get('/', function (req, res) {
-  res.send('<h1>Hello World</h1>')
-})
+app.use(express.static(__dirname + '/public'))
  
 app.listen(3000)
